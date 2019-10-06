78cb762d50c1f9523e7e19607c33b48775329138
hatt0519 <moroku0519@gmail.com>
2019-10-06 18:09:25 +0900

    アニメーションのサンプル実装

--- app/src/main/java/xyz/moroku0519/gugen2019/MainActivity.kt
+++ app/src/main/java/xyz/moroku0519/gugen2019/MainActivity.kt
@@ -1,12 +1,39 @@
 package xyz.moroku0519.gugen2019
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.Animator
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rainy.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(p0: Animator?) {
+            }
+
+            override fun onAnimationEnd(p0: Animator?) {
+                rainy.startAnimation(
+                    AnimationUtils.loadAnimation(this@MainActivity, R.anim.fade_out)
+                )
+            }
+
+            override fun onAnimationCancel(p0: Animator?) {
+            }
+
+            override fun onAnimationStart(p0: Animator?) {
+                rainy.startAnimation(
+                    AnimationUtils.loadAnimation(this@MainActivity, R.anim.fade_in)
+                )
+            }
+
+        }
+        )
+        send.setOnClickListener {
+            rainy.playAnimation()
+        }
     }
 }
