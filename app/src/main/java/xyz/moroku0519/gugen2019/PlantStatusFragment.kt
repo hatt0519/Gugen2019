053519c372a407470a29eaf3a6a2ef60289bc689
hatt0519 <moroku0519@gmail.com>
2019-10-07 07:40:16 +0900

    extract instance

--- app/src/main/java/xyz/moroku0519/gugen2019/PlantStatusFragment.kt
+++ app/src/main/java/xyz/moroku0519/gugen2019/PlantStatusFragment.kt
@@ -10,31 +10,49 @@ import androidx.navigation.fragment.NavHostFragment
 import kotlinx.android.synthetic.main.fragment_plant_status.view.*
 
 class PlantStatusFragment : NavHostFragment() {
+
+    private val View.startEndAnimatorListener: StartEndAnimatorListener
+        get() = StartEndAnimatorListener(
+            {
+                this.rainy.startAnimation(
+                    AnimationUtils.loadAnimation(requireContext(), R.anim.fade_out)
+                )
+                message.text = "ありがとう！！"
+            },
+            {
+                this.rainy.startAnimation(
+                    AnimationUtils.loadAnimation(requireContext(), R.anim.fade_in)
+                )
+            }
+        )
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
         inflater.inflate(R.layout.fragment_plant_status, container, false).apply {
-            this.rainy.addAnimatorListener(object : Animator.AnimatorListener {
-                override fun onAnimationRepeat(p0: Animator?) {
-                }
-
-                override fun onAnimationEnd(p0: Animator?) {
-                    this@apply.rainy.startAnimation(
-                        AnimationUtils.loadAnimation(requireContext(), R.anim.fade_out)
-                    )
-                    message.text = "ありがとう！！"
-                }
-
-                override fun onAnimationCancel(p0: Animator?) {
-                }
-
-                override fun onAnimationStart(p0: Animator?) {
-                    this@apply.rainy.startAnimation(
-                        AnimationUtils.loadAnimation(requireContext(), R.anim.fade_in)
-                    )
-                }
-            })
+            this.rainy.addAnimatorListener(startEndAnimatorListener)
             this.send.setOnClickListener {
                 this.rainy.playAnimation()
             }
         }
+
+    private class StartEndAnimatorListener(
+        private val onAnimationStart: () -> Unit,
+        private val onAnimationEnd: () -> Unit
+    ) : Animator.AnimatorListener {
+        override fun onAnimationRepeat(p0: Animator?) {
+        }
+
+        override fun onAnimationEnd(p0: Animator?) {
+            onAnimationStart()
+        }
+
+        override fun onAnimationCancel(p0: Animator?) {
+        }
+
+        override fun onAnimationStart(p0: Animator?) {
+            onAnimationEnd()
+        }
+    }
+
 }
 
