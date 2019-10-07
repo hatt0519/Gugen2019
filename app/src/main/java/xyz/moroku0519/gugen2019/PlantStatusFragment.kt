e927f52c6e848bec2c66a1222e578bd3c3565a1a
hatt0519 <moroku0519@gmail.com>
2019-10-08 07:30:27 +0900

    navigationで水やりの表現

--- app/src/main/java/xyz/moroku0519/gugen2019/PlantStatusFragment.kt
+++ app/src/main/java/xyz/moroku0519/gugen2019/PlantStatusFragment.kt
@@ -1,58 +1,21 @@
 package xyz.moroku0519.gugen2019
 
-import android.animation.Animator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.AnimationUtils
-import androidx.navigation.fragment.NavHostFragment
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.fragment_plant_status.view.*
 
-class PlantStatusFragment : NavHostFragment() {
-
-    private val View.startEndAnimatorListener: StartEndAnimatorListener
-        get() = StartEndAnimatorListener(
-            {
-                this.rainy.startAnimation(
-                    AnimationUtils.loadAnimation(requireContext(), R.anim.fade_out)
-                )
-                message.text = "ありがとう！！"
-            },
-            {
-                this.rainy.startAnimation(
-                    AnimationUtils.loadAnimation(requireContext(), R.anim.fade_in)
-                )
-            }
-        )
-
+class PlantStatusFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
         inflater.inflate(R.layout.fragment_plant_status, container, false).apply {
-            this.rainy.addAnimatorListener(startEndAnimatorListener)
             this.send.setOnClickListener {
-                this.rainy.playAnimation()
+                it.findNavController().navigate(R.id.action_plant_status_to_water)
             }
         }
-
-    private class StartEndAnimatorListener(
-        private val onAnimationStart: () -> Unit,
-        private val onAnimationEnd: () -> Unit
-    ) : Animator.AnimatorListener {
-        override fun onAnimationRepeat(p0: Animator?) {
-        }
-
-        override fun onAnimationEnd(p0: Animator?) {
-            onAnimationStart()
-        }
-
-        override fun onAnimationCancel(p0: Animator?) {
-        }
-
-        override fun onAnimationStart(p0: Animator?) {
-            onAnimationEnd()
-        }
-    }
-
 }
+
 
---
+++ app/src/main/java/xyz/moroku0519/gugen2019/WaterFragment.kt
@@ -0,0 +1,53 @@
+package xyz.moroku0519.gugen2019
+
+import android.animation.Animator
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_give_water.view.*
+
+class WaterFragment : Fragment() {
+    private val View.startEndAnimatorListener: StartEndAnimatorListener
+        get() = StartEndAnimatorListener(
+            {
+                water.startAnimation(AnimationUtils.loadAnimation(requireContext(), R.anim.fade_in))
+            },
+            {
+                water.startAnimation(AnimationUtils.loadAnimation(requireContext(), R.anim.fade_out))
+                findNavController().popBackStack(R.id.plant_status, false)
+            }
+        )
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+        inflater.inflate(R.layout.fragment_give_water, container, false).apply {
+            water.addAnimatorListener(startEndAnimatorListener)
+        }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.water.playAnimation()
+    }
+
+    private class StartEndAnimatorListener(
+        private val onAnimationStart: () -> Unit,
+        private val onAnimationEnd: () -> Unit
+    ) : Animator.AnimatorListener {
+        override fun onAnimationRepeat(p0: Animator?) {
+        }
+
+        override fun onAnimationEnd(p0: Animator?) {
+            onAnimationEnd()
+        }
+
+        override fun onAnimationCancel(p0: Animator?) {
+        }
+
+        override fun onAnimationStart(p0: Animator?) {
+            onAnimationStart()
+        }
+    }
+}
