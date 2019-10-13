7dae827b466f03ebbea588a997ae98e4b0e050c3
hatt0519 <moroku0519@gmail.com>
2019-10-13 14:03:27 +0900

    inline

--- app/src/main/java/xyz/moroku0519/gugen2019/data/CommandRepositoryImpl.kt
+++ app/src/main/java/xyz/moroku0519/gugen2019/data/CommandRepositoryImpl.kt
@@ -1,16 +1,16 @@
 package xyz.moroku0519.gugen2019.data
 
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 class CommandRepositoryImpl : CommandRepository {
     // TODO: applicationでひとつだけもたせるようにしたい
     private val firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
-    private val ref: DatabaseReference = firebaseDatabase.getReference("isWaterNeed")
+
     override fun sendWater() {
-        ref.setValue(true)
+        firebaseDatabase.getReference("isWaterNeed").setValue(true)
     }
 
     override fun sendSunLight() {
+        firebaseDatabase.getReference("isSunlightNeed").setValue(true)
     }
 }
