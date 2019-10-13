106426bb2f71d51ceb08b0506170ef1fb62ec392
hatt0519 <moroku0519@gmail.com>
2019-10-13 14:54:06 +0900

    property定義

--- app/src/main/java/xyz/moroku0519/gugen2019/data/StatusRepository.kt
+++ app/src/main/java/xyz/moroku0519/gugen2019/data/StatusRepository.kt
@@ -1,12 +1,15 @@
 package xyz.moroku0519.gugen2019.data
 
+import androidx.annotation.DrawableRes
+import xyz.moroku0519.gugen2019.R
+
 interface StatusRepository {
     fun loadGirlStatus(): GirlStatus
-    fun loadGirlLoveMeter() : Int
+    fun loadGirlLoveMeter(): Int
 
-    enum class GirlStatus(val image: String) {
-        GOOD(""),
-        BAD(""),
-        NORMAL("")
+    enum class GirlStatus(@DrawableRes val drawableId: Int) {
+        GOOD(R.drawable.good),
+        BAD(R.drawable.bad),
+        NORMAL(R.drawable.normal)
     }
 }
