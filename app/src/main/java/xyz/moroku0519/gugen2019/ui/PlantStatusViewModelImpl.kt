c125d25710a4d3084583b406dd918ace05beff82
hatt0519 <moroku0519@gmail.com>
2019-10-13 15:09:51 +0900

    決め打ちでimpl

---
+++ app/src/main/java/xyz/moroku0519/gugen2019/ui/PlantStatusViewModelImpl.kt
@@ -0,0 +1,27 @@
+package xyz.moroku0519.gugen2019.ui
+
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.navigation.findNavController
+import xyz.moroku0519.gugen2019.R
+import xyz.moroku0519.gugen2019.data.CommandRepository
+import xyz.moroku0519.gugen2019.data.CommandRepositoryImpl
+import xyz.moroku0519.gugen2019.data.StatusRepository
+
+class PlantStatusViewModelImpl : PlantStatusViewModel {
+    private val commandRepository: CommandRepository = CommandRepositoryImpl()
+    override val plantStatus: LiveData<StatusRepository.GirlStatus> =
+        MutableLiveData(StatusRepository.GirlStatus.NORMAL)
+    override val isWaterButtonVisible: LiveData<Boolean> = MutableLiveData(true)
+    override val isSunlightButtonVisible: LiveData<Boolean> = MutableLiveData(false)
+    override val loveMeterParameter: LiveData<Float> = MutableLiveData(4.0f)
+
+    override fun onWaterButtonClick(v: View) {
+        v.findNavController().navigate(R.id.action_plant_status_to_water)
+        commandRepository.sendWater()
+    }
+
+    override fun onSunlightButtonClick(v: View) {
+    }
+}
