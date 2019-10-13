8099b58945ed976492025d0ad430f89c129e988c
hatt0519 <moroku0519@gmail.com>
2019-10-13 15:29:23 +0900

    DataBIndingに対応

--- app/src/main/java/xyz/moroku0519/gugen2019/ui/PlantStatusFragment.kt
+++ app/src/main/java/xyz/moroku0519/gugen2019/ui/PlantStatusFragment.kt
@@ -5,26 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
-import kotlinx.android.synthetic.main.fragment_plant_status.view.*
-import xyz.moroku0519.gugen2019.R
 import xyz.moroku0519.gugen2019.data.CommandRepository
 import xyz.moroku0519.gugen2019.data.CommandRepositoryImpl
+import xyz.moroku0519.gugen2019.databinding.FragmentPlantStatusBinding
 
 class PlantStatusFragment : Fragment() {
     private val args: PlantStatusFragmentArgs by navArgs()
     private val commandRepository: CommandRepository = CommandRepositoryImpl()
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.fragment_plant_status, container, false).apply {
-            this.send.setOnClickListener {
-                it.findNavController().navigate(R.id.action_plant_status_to_water)
-                commandRepository.sendWater()
-            }
-            args.message?.let {
-                this.message.text = it
-            }
-        }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? =
+        FragmentPlantStatusBinding.inflate(inflater, container, false).apply {
+            viewModel = PlantStatusViewModelImpl()
+        }.root
+
 }
 
 
