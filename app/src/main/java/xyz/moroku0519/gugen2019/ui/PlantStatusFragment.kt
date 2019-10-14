151896aaed600b610c02433dc0562f68157da134
hatt0519 <moroku0519@gmail.com>
2019-10-14 21:53:57 +0900

    ViewModel側に移動

--- app/src/main/java/xyz/moroku0519/gugen2019/ui/PlantStatusFragment.kt
+++ app/src/main/java/xyz/moroku0519/gugen2019/ui/PlantStatusFragment.kt
@@ -1,20 +1,16 @@
 package xyz.moroku0519.gugen2019.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import xyz.moroku0519.gugen2019.GugenApplication
-import xyz.moroku0519.gugen2019.data.GirlsRepository
-import xyz.moroku0519.gugen2019.data.GirlsRepositoryImpl
 import xyz.moroku0519.gugen2019.databinding.FragmentPlantStatusBinding
 
 class PlantStatusFragment : Fragment() {
     private val args: PlantStatusFragmentArgs by navArgs()
-    private val girlsRepository: GirlsRepository = GirlsRepositoryImpl()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,15 +18,7 @@ class PlantStatusFragment : Fragment() {
     ): View? =
         FragmentPlantStatusBinding.inflate(inflater, container, false).apply {
             viewModel = PlantStatusViewModelImpl(GugenApplication.application).apply {
-                girlsRepository.loadGirl(
-                    { girl ->
-                        plantStatus.postValue(girl.girlStatus)
-                    },
-                    {
-                        Log.e("error", it?.message)
-                    }
-                )
-//                plantStatus.postValue(args.girlStatus)
+                plantStatus.postValue(args.girlStatus)
             }
             lifecycleOwner = viewLifecycleOwner
         }.root
--- app/src/main/java/xyz/moroku0519/gugen2019/ui/PlantStatusViewModelImpl.kt
+++ app/src/main/java/xyz/moroku0519/gugen2019/ui/PlantStatusViewModelImpl.kt
@@ -2,6 +2,7 @@ package xyz.moroku0519.gugen2019.ui
 
 import android.app.Application
 import android.graphics.drawable.Drawable
+import android.util.Log
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.AndroidViewModel
@@ -13,13 +14,15 @@ import xyz.moroku0519.gugen2019.GugenApplication
 import xyz.moroku0519.gugen2019.R
 import xyz.moroku0519.gugen2019.data.CommandRepository
 import xyz.moroku0519.gugen2019.data.CommandRepositoryImpl
+import xyz.moroku0519.gugen2019.data.GirlsRepository
+import xyz.moroku0519.gugen2019.data.GirlsRepositoryImpl
 import xyz.moroku0519.gugen2019.data.entity.GirlStatus
 
 class PlantStatusViewModelImpl(application: Application) : PlantStatusViewModel,
     AndroidViewModel(application) {
     private val commandRepository: CommandRepository = CommandRepositoryImpl()
-    override val plantStatus: MutableLiveData<GirlStatus> =
-        MutableLiveData(GirlStatus.NORMAL)
+    private val girlsRepository: GirlsRepository = GirlsRepositoryImpl()
+    override val plantStatus: MutableLiveData<GirlStatus> = MutableLiveData()
     override val girlImage: LiveData<Drawable> = Transformations.map(plantStatus) {
         ContextCompat.getDrawable(getApplication<GugenApplication>(), it.drawableId)
     }
@@ -36,5 +39,13 @@ class PlantStatusViewModelImpl(application: Application) : PlantStatusViewModel,
     }
 
     override fun onSunlightButtonClick(v: View) {
+    }
+
+    init {
+        // TODO:本当は一発でLiveData変換したい...
+        girlsRepository.loadGirl(
+            { girl -> plantStatus.postValue(girl.girlStatus) },
+            { e -> Log.e("error", e?.message) }
+        )
     }
 }
