b5994295d3fc715996750bbf03beb2d3bfe0f3ad
hatt0519 <moroku0519@gmail.com>
2019-10-22 22:42:16 +0900

    引数によってアニメーション切り替え

--- app/src/main/java/xyz/moroku0519/gugen2019/ui/LoadingFragment.kt
+++ app/src/main/java/xyz/moroku0519/gugen2019/ui/LoadingFragment.kt
@@ -8,11 +8,15 @@ import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.fragment_loading.view.*
 import xyz.moroku0519.gugen2019.R
 import xyz.moroku0519.gugen2019.data.entity.GirlStatus
 
 class LoadingFragment : Fragment() {
+
+    private val args: LoadingFragmentArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,6 +25,13 @@ class LoadingFragment : Fragment() {
         inflater.inflate(R.layout.fragment_loading, container, false).apply {
             loading.addAnimatorListener(startEndAnimatorListener)
             loading.setAnimation(R.raw.water)
+            loading.setAnimation(
+                when (args.girlStatus) {
+                    GirlStatus.POOR_SUNLIGHT -> R.raw.sunlight
+                    GirlStatus.POOR_WATER -> R.raw.water
+                    else -> throw IllegalArgumentException("IllegalGirlStatus:" + args.girlStatus.toString())
+                }
+            )
         }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
