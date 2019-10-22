c7a4bfabc72505b863625a4cfd9503581023a5c1
hatt0519 <moroku0519@gmail.com>
2019-10-22 22:08:58 +0900

    sunlight追加

--- app/src/main/java/xyz/moroku0519/gugen2019/data/entity/GirlStatus.kt
+++ app/src/main/java/xyz/moroku0519/gugen2019/data/entity/GirlStatus.kt
@@ -4,16 +4,20 @@ import androidx.annotation.DrawableRes
 import xyz.moroku0519.gugen2019.R
 
 enum class GirlStatus(@DrawableRes val drawableId: Int, val message: String) {
+    // TODO: セリフはローカルで管理しないほうがいい気がする。ボキャブラリを追加しにくい。
     GOOD(R.drawable.good, "ありがとう！"),
-    BAD(R.drawable.bad, "お水が欲しいよ〜"),
+    POOR_WATER(R.drawable.bad, "お水が欲しいよ〜"),
+    POOR_SUNLIGHT(R.drawable.bad, "日光浴したい〜"),
     NORMAL(R.drawable.normal, "ご機嫌いかが？");
 
     companion object {
+        // TODO: 対応するidがクライアントとサーバでバラバラ管理なのを何とかする
         fun fromId(id: Int): GirlStatus =
             when (id) {
                 0 -> GOOD
-                1 -> BAD
-                2 -> NORMAL
+                1 -> POOR_WATER
+                2 -> POOR_SUNLIGHT
+                3 -> NORMAL
                 else -> throw IllegalArgumentException("illegalId")
             }
     }
