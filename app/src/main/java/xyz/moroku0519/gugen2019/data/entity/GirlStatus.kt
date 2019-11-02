4095b90b9c2c213bb48a71d435e48980fd133f61
hatt0519 <moroku0519@gmail.com>
2019-11-02 15:23:34 +0900

    id追加

--- app/src/main/java/xyz/moroku0519/gugen2019/data/entity/GirlStatus.kt
+++ app/src/main/java/xyz/moroku0519/gugen2019/data/entity/GirlStatus.kt
@@ -3,22 +3,22 @@ package xyz.moroku0519.gugen2019.data.entity
 import androidx.annotation.DrawableRes
 import xyz.moroku0519.gugen2019.R
 
-enum class GirlStatus(@DrawableRes val drawableId: Int, val message: String) {
+enum class GirlStatus(val id: Int, @DrawableRes val drawableId: Int, val message: String) {
     // TODO: セリフはローカルで管理しないほうがいい気がする。ボキャブラリを追加しにくい。
-    GOOD(R.drawable.good, "ありがとう！"),
-    POOR_WATER(R.drawable.bad, "お水が欲しいよ〜"),
-    POOR_SUNLIGHT(R.drawable.bad, "日光浴したい〜"),
-    NORMAL(R.drawable.normal, "ご機嫌いかが？");
+    GOOD(0, R.drawable.good, "ありがとう！"),
+    POOR_WATER(1, R.drawable.bad, "お水が欲しいよ〜"),
+    POOR_SUNLIGHT(2, R.drawable.bad, "日光浴したい〜"),
+    NORMAL(3, R.drawable.normal, "ご機嫌いかが？");
 
     companion object {
         // TODO: 対応するidがクライアントとサーバでバラバラ管理なのを何とかする
         fun fromId(id: Int): GirlStatus =
-            when (id) {
-                0 -> GOOD
-                1 -> POOR_WATER
-                2 -> POOR_SUNLIGHT
-                3 -> NORMAL
-                else -> throw IllegalArgumentException("illegalId")
-            }
+                when (id) {
+                    0 -> GOOD
+                    1 -> POOR_WATER
+                    2 -> POOR_SUNLIGHT
+                    3 -> NORMAL
+                    else -> throw IllegalArgumentException("illegalId")
+                }
     }
 }
